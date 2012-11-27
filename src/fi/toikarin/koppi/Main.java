a1413b2b91c8dc06bf825a1a434658f90b52362f
Tomi Oikarinen <tomi.oikarinen@iki.fi>
2012-11-27 22:22:49 +0200

    clean eclipse's indenting

--- src/fi/toikarin/koppi/Main.java
+++ src/fi/toikarin/koppi/Main.java
@@ -7,20 +7,20 @@ import android.app.Application;
 public class Main extends Application {
     private Integer lastCount = null;
     private Calendar lastUpdated = null;
-    
+
     public Integer getLastCount() {
-    	return lastCount;
+        return lastCount;
     }
-    
+
     public void setLastCount(int lastCount) {
-    	this.lastCount = lastCount;
+        this.lastCount = lastCount;
     }
-    
+
     public void setLastUpdated(Calendar lastUpdated) {
-    	this.lastUpdated = lastUpdated;
+        this.lastUpdated = lastUpdated;
     }
-    
+
     public Calendar getLastUpdated() {
-    	return lastUpdated;
+        return lastUpdated;
     }
 }
--- src/fi/toikarin/koppi/MainActivity.java
+++ src/fi/toikarin/koppi/MainActivity.java
@@ -31,11 +31,11 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	private TextView playerCountTextView;
-	private TextView checkStatusTextView;
+    private TextView playerCountTextView;
+    private TextView checkStatusTextView;
     private Button updateButton;
     private ProgressBar progressBar;
- 
+
     private Main main;
     private Timer timer = new Timer();
     private int counter = 0;
@@ -47,23 +47,22 @@ public class MainActivity extends Activity {
     private static final String url = "http://peku.kapsi.fi/koppi.php";
     private static final Pattern patternWaiting = Pattern.compile("<p>(\\d) pelaajaa valmiina</p>");
     private static final SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");
-    
+
     public int testPlayerCount() {
-    	int old = ++counter;
-    	
-    	switch (old) {
-    	case 7:
-    		return 5;
-    	case 8:
-    		return 4;
-    	case 9:
-    		return 5;
-    		default:
-    			return old;
-    	
-    	}
+        int old = ++counter;
+
+        switch (old) {
+            case 7:
+                return 5;
+            case 8:
+                return 4;
+            case 9:
+                return 5;
+            default:
+                return old;
+        }
     }
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -73,58 +72,58 @@ public class MainActivity extends Activity {
         playerCountTextView = (TextView) findViewById(R.id.status);
         checkStatusTextView = (TextView) findViewById(R.id.textView);
         updateButton = (Button) findViewById(R.id.updateButton);
-        
+
         updateButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				if (canUpdate()) {
-					myClickHandler(view);
-				}
-			}
-		});
-        
+            @Override
+            public void onClick(View view) {
+                if (canUpdate()) {
+                    myClickHandler(view);
+                }
+            }
+        });
+
         main = (Main) getApplicationContext();
-        
+
         timer.scheduleAtFixedRate(new UpdateTask(), 0, updateInterval);
-        
+
         setCheckStatus();
         if (main.getLastCount() != null) {
-        	setReadyCount(main.getLastCount());
+            setReadyCount(main.getLastCount());
         }
     }
-    
+
     @Override
     public void onPause() {
-    	if (timer != null) {
-    	    timer.cancel();
-    	}
-    	
-    	Log.i(TAG, "pause");
-    	
-    	super.onPause();
-    }
-    
-    public boolean canUpdate() {
-    	if (main.getLastUpdated() == null) {
-    		return true;
-    	}
-    	
-    	Calendar calendar = Calendar.getInstance();
-    	calendar.add(Calendar.MILLISECOND, -updateThreshold);
-    	
-    	return calendar.after(main.getLastUpdated());
-    }
-    
-    public boolean shouldUpdate() {
-    	Log.w(TAG, "" + main.getLastUpdated());
-    	if (main.getLastUpdated() == null) {
-    		return true;
-    	}
+        if (timer != null) {
+            timer.cancel();
+        }
 
-    	Calendar calendar = Calendar.getInstance();
-    	calendar.add(Calendar.MILLISECOND, -updateInterval);
-    	
-    	return calendar.after(main.getLastUpdated());
+        Log.i(TAG, "pause");
+
+        super.onPause();
+    }
+
+    public boolean canUpdate() {
+        if (main.getLastUpdated() == null) {
+            return true;
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MILLISECOND, -updateThreshold);
+
+        return calendar.after(main.getLastUpdated());
+    }
+
+    public boolean shouldUpdate() {
+        Log.w(TAG, "" + main.getLastUpdated());
+        if (main.getLastUpdated() == null) {
+            return true;
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MILLISECOND, -updateInterval);
+
+        return calendar.after(main.getLastUpdated());
     }
 
     @Override
@@ -139,44 +138,44 @@ public class MainActivity extends Activity {
 
         if (networkInfo != null && networkInfo.isConnected()) {
             MainActivity.this.runOnUiThread(new Runnable() {
-            	@Override
-            	public void run() {
-            		new DownloadKoppiStatus().execute();
-            	}
+                @Override
+                public void run() {
+                    new DownloadKoppiStatus().execute();
+                }
             });
         } else {
             playerCountTextView.setText("No network connection available.");
         }
     }
-    
-    private class UpdateTask extends TimerTask {
-		@Override
-		public void run() {
 
-			if (shouldUpdate()) {
-				Log.d(TAG, "TimerTask.run() " + df.format(new Date()));
-				myClickHandler(null);
-			}
-		}
+    private class UpdateTask extends TimerTask {
+        @Override
+        public void run() {
+
+            if (shouldUpdate()) {
+                Log.d(TAG, "TimerTask.run() " + df.format(new Date()));
+                myClickHandler(null);
+            }
+        }
     }
-    
+
     private void setCheckStatus() {
-    	if (main.getLastUpdated() == null) {
-    		return;
-    	}
-   
-		checkStatusTextView.setText(getResources().getString(R.string.last_checked) + ":\n"
-				+ df.format(main.getLastUpdated().getTime()));
+        if (main.getLastUpdated() == null) {
+            return;
+        }
+
+        checkStatusTextView.setText(getResources().getString(R.string.last_checked) + ":\n"
+                + df.format(main.getLastUpdated().getTime()));
     }
-    
+
     private void setReadyCount(int readyCount) {
         String newText = String.format(getResources().getString(R.string.players_ready), readyCount);
         playerCountTextView.setText(newText);
-        
+
         if (readyCount >= 5) {
-        	playerCountTextView.setBackgroundColor(0xFF99FF99);
+            playerCountTextView.setBackgroundColor(0xFF99FF99);
         } else {
-        	playerCountTextView.setBackgroundColor(0xFFFFFF99);
+            playerCountTextView.setBackgroundColor(0xFFFFFF99);
         }
     }
 
@@ -185,18 +184,18 @@ public class MainActivity extends Activity {
         protected String doInBackground(Object... params) {
 
             MainActivity.this.runOnUiThread(new Runnable() {
-				@Override
-				public void run() {
-		            progressBar.setVisibility(ProgressBar.VISIBLE);
-				}
-			});
+                @Override
+                public void run() {
+                    progressBar.setVisibility(ProgressBar.VISIBLE);
+                }
+            });
 
-        	try {
-        		if (!debug) {
-        			return download();
-        		} else {
-        			return "<p>" + testPlayerCount() + " pelaajaa valmiina</p>";
-        		}
+            try {
+                if (!debug) {
+                    return download();
+                } else {
+                    return "<p>" + testPlayerCount() + " pelaajaa valmiina</p>";
+                }
             } catch (IOException e) {
                 return "Unable to retrieve web page. URL may be invalid.";
             }
@@ -204,28 +203,28 @@ public class MainActivity extends Activity {
 
         @Override
         protected void onPostExecute(String result) {
-        	Log.d(TAG, "Received: " + result);
-        	
+            Log.d(TAG, "Received: " + result);
+
             final int readyCount = parse(result);
             Integer lastCount = main.getLastCount();
-            
+
             main.setLastCount(readyCount);
             main.setLastUpdated(Calendar.getInstance());
 
             MainActivity.this.runOnUiThread(new Runnable() {
-				@Override
-				public void run() {
-					progressBar.setVisibility(ProgressBar.GONE);
-					
-					setCheckStatus();
-					setReadyCount(readyCount);
-				}
+                @Override
+                public void run() {
+                    progressBar.setVisibility(ProgressBar.GONE);
 
-			});
-            
+                    setCheckStatus();
+                    setReadyCount(readyCount);
+                }
+
+            });
+
             if (readyCount >= 5 && (lastCount == null || lastCount < 5)) {
-            	MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.koppi);
-				mp.start();
+                MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.koppi);
+                mp.start();
             }
         }
 
@@ -234,15 +233,15 @@ public class MainActivity extends Activity {
             if (matcher.find()) {
                 return Integer.parseInt(matcher.group(1));
             }
-            
+
             return -1;
         }
 
         private String download() throws IOException {
             InputStream is = null;
-            
+
             Log.d(TAG, "Downloading: " + url);
-                  
+
             try {
                 URL u = new URL(url);
                 HttpURLConnection conn = (HttpURLConnection) u.openConnection();
@@ -252,37 +251,37 @@ public class MainActivity extends Activity {
                 conn.setDoInput(true);
 
                 conn.connect();
-                              
+
                 if (conn.getResponseCode() != 200) {
                     throw new NullPointerException();
                 }
-                  
+
                 is = conn.getInputStream();
                 return readIt(is);
             } finally {
                 if (is != null) {
                     is.close();
-                } 
+                }
             }
         }
-            
+
         public String readIt(InputStream stream) throws IOException, UnsupportedEncodingException {
             BufferedReader in = null;
             StringBuilder sb = new StringBuilder();
-            
+
             try {
-            	in = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
-            	String str;
+                in = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
+                String str;
 
                 while ((str = in.readLine()) != null) {
-                   sb.append(str).append('\n');
+                    sb.append(str).append('\n');
                 }
             } finally {
-            	if (in != null) {
-            		in.close();
-            	}
+                if (in != null) {
+                    in.close();
+                }
             }
-            
+
             return sb.toString();
         }
     }
