d9575552d6139be08e7201c8c67b2422a8b64642
Tomi Oikarinen <tomi.oikarinen@iki.fi>
2012-11-28 13:55:03 +0200

    Added ACRA.

--- src/fi/toikarin/koppi/Main.java
+++ src/fi/toikarin/koppi/Main.java
@@ -4,9 +4,24 @@ import java.util.Calendar;
 
 import android.app.Application;
 
+import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+
+@ReportsCrashes(formKey = "",
+        mailTo="tomi.oikarinen@iki.fi",
+        mode = ReportingInteractionMode.TOAST,
+        resToastText=R.string.crash_toast_text)
 public class Main extends Application {
     private Integer lastCount = null;
     private Calendar lastUpdated = null;
+
+    @Override
+	public void onCreate() {
+		super.onCreate();
+
+		ACRA.init(this);
+	}
 
     public Integer getLastCount() {
         return lastCount;
